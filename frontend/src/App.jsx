7041519336c5a27ea1b332f91e541e3408e65549
Prashant Kumar Singh <prashant090798@gmail.com>
2024-09-08 22:47:48 +0530

    added : jwt, oauth

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+function App() {
+  const { loginWithRedirect, isAuthenticated, user, logout } = useAuth0();
+
+  return (
+    <>
+      <h1>Home Page</h1>
+      {isAuthenticated ? (
+        <div className="card">
+          <p>Welcome {user.name}</p>
+          <button
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div className="card">
+          <p>Please Login</p>
+          <button onClick={() => loginWithRedirect()}>Login</button>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
