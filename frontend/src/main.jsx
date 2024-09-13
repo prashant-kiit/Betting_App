af32d194b2c8e5f3639f167b61b72cd5a0d77e89
Prashant Kumar Singh <prashant090798@gmail.com>
2024-09-13 15:53:50 +0530

    deleted : node oauth frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,15 +6,12 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Layout from "../Layout.jsx";
 import Home from "./Home.jsx";
 import Dashboard from "./Dashboard.jsx";
 import Logout from "./Logout.jsx";
 import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App.jsx";
-
-const queryClient = new QueryClient();
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -29,22 +26,18 @@ const router = createBrowserRouter(
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-  </QueryClientProvider>
+  <Auth0Provider
+    domain="dev-vl5s6zif0qwkov3t.us.auth0.com"
+    clientId="uAITTrLPvxOwr0jyyWyHborRd5yEZCNQ"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <App />
+  </Auth0Provider>
   // </StrictMode>
 );
 
 {
-  /* <Auth0Provider
-  domain="dev-vl5s6zif0qwkov3t.us.auth0.com"
-  clientId="uAITTrLPvxOwr0jyyWyHborRd5yEZCNQ"
-  authorizationParams={{
-    redirect_uri: window.location.origin,
-  }}
-  ></Auth0Provider>; */
-}
-
-{
-  /* <App /> */
+  // <RouterProvider router={router} />
 }
