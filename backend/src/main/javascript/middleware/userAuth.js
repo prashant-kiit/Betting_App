6df4dff064b2327e969d9db01864932977efd7e3
Prashant Kumar Singh <prashant090798@gmail.com>
2024-09-13 15:45:41 +0530

    deleted: node oauth routes

--- backend/src/main/javascript/middleware/userAuth.js
+++
@@ -1,7 +0,0 @@
-export const userAuth = (req, res, next) => {
-  if (req.oidc.isAuthenticated()) {
-    next();
-  } else {
-    return res.status(301).redirect("http://localhost:8081/app/logout");
-  }
-};
